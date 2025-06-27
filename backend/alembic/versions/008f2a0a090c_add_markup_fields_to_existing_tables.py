28cada25b79ac0e329258aae546d730bbcc5b9de
Tony Guinta <tonyguinta@gmail.com>
2025-06-27 01:46:20 -0500

    Fix migration dependency: Set down_revision to None since base migration was removed

--- backend/alembic/versions/008f2a0a090c_add_markup_fields_to_existing_tables.py
+++ backend/alembic/versions/008f2a0a090c_add_markup_fields_to_existing_tables.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '008f2a0a090c'
-down_revision: Union[str, None] = '6087350756df'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
